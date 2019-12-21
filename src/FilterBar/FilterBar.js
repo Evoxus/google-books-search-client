c05bab4dcea2d3326bee68152ef72f824b84060d
Evoxus <michael@michaelhploughman.com>
2019-12-21 15:28:12 -0700

    Tweaked filter options

--- src/FilterBar/FilterBar.js
+++ src/FilterBar/FilterBar.js
@@ -6,15 +6,13 @@ export default function FilterBar(props) {
       <div>
         <label htmlFor='printTypeSelect'>Print Type: </label>
         <select name='printType' id='printTypeSelect'>
-          <option value='all'>--Please choose an option--</option>
-          <option value='all'>All</option>
+          <option value='all'>No Filter</option>
           <option value='books'>Books</option>
           <option value='magazines'>Magazines</option>
         </select>
         <label htmlFor='bookTypeSelect'>Book Type: </label>
         <select name='bookType' id='bookTypeSelect'>
-          <option value='ebooks'>--Please choose an option--</option>
-          <option value='ebooks'>All</option>
+          <option value='ebooks'>No Filter</option>
           <option value='free-ebooks'>Free eBooks</option>
           <option value='paid-ebooks'>Paid eBooks</option>
         </select>
